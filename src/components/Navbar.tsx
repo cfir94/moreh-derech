b3e0a175297ef278053259d7b1fab71d38054f2e
cfir94 <94324283+cfir94@users.noreply.github.com>
2026-09-04 10:57:48 +0300

    Add game link to top navigation

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,6 +6,7 @@ import { useState } from "react";
 import { useUser } from "@/contexts/UserContext";
 
 const links = [
+  { href: "https://cfir94.github.io/israel-geo-game/", label: "המשחק" },
   { href: "/quizzes", label: "שאלונים" },
   { href: "/timelines", label: "צירי זמן" },
   { href: "/map", label: "מפה" },
